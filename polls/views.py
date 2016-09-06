55a5fbb58c38ff2f161f74dab9a49b9fff5d018a
NOEL_DAVID <NOEL_DAVID@NOELDASUS>
2016-09-06 23:23:04 +0800

    Changes to view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Noel David S");
-    
+    return HttpResponse("Hello Noel David S")
+
+def test():
+    return HttpResponse("Test")
